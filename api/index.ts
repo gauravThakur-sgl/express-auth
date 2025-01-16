c61445e488e74ae08f5c9c283e96401e247f953c
gauravThakur-sgl <gaurav.thakur@shipglobal.in>
2025-01-16 19:13:31 +0530

    Added db connection

--- api/index.ts
+++ api/index.ts
@@ -15,13 +15,23 @@ import entriesRoute from "../src/routes/entries.route";
 import userRouter from "../src/routes/userRoute";
 import eventRouter from "../src/routes/eventRoute";
 
+const connectToDB = require("../src/config/connectToDb");
+
+const port = process.env.PORT || 3001;
+// Connect to database
+connectToDB();
+
 // Use routes
-app.use("/entries", entriesRoute);
-app.use("/users", userRouter);
-app.use("/events", eventRouter);
+app.use("/api/entries", entriesRoute);
+app.use("/api/users", userRouter);
+app.use("/api/events", eventRouter);
 
 app.get("/", (req, res) => {
   res.json("Welcome to the API");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port 8081`);
 });
 
 export default app;
