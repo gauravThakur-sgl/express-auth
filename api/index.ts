b08de934bde589d5267051ed62d6d5c13907f4aa
gauravThakur-sgl <gaurav.thakur@shipglobal.in>
2025-01-16 20:23:14 +0530

    Removed config file

--- api/index.ts
+++ api/index.ts
@@ -1,37 +1,42 @@
+const express = require("express");
 import { Request, Response } from "express";
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+const path = require("path");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const entriesRoute = require("../src/routes/entries.route").default;
 const userRouter = require("../src/routes/userRoute").default;
 const eventRouter = require("../src/routes/eventRoute").default;
-
 dotenv.config();
 const app = express();
-
 const port = process.env.PORT || 3001;
 require("dotenv").config();
-
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 
-const connectToDB = require("../src/config/connectToDb").default;
+//connect database
 
-// Connect to database
-connectToDB();
+const mongoUri = process.env.MONGODB_URI;
 
+if (mongoUri) {
+  mongoose.connect(mongoUri).catch((err: any) => console.log(err));
+} else {
+  console.error("MONGODB_URI is not defined in the environment variables");
+}
 app.get("/", (req: Request, res: Response) => {
-  res.json("Welcome to the API");
+  res.json({ message: "Hello World" });
 });
 
-// Use routes
-app.use("/api/entries", entriesRoute);
+app.use("/entries", entriesRoute);
 app.use("/api/users", userRouter);
 app.use("/api/events", eventRouter);
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
-  console.log(`Server is running on port 8081`);
+  console.log(`Connected to DB and app listening on port ${port}`);
 });
-
-export default app;
